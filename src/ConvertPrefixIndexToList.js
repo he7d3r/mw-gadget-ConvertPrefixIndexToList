0b86e377512141f40e060575c636fb48af83e808
Helder <he7d3r@gmail.com>
2012-03-06 19:11:04 +0000

    Test if "filterLists.load" and "filterLists.buildForm" are functions

--- src/ConvertPrefixIndexToList.js
+++ src/ConvertPrefixIndexToList.js
@@ -3,14 +3,14 @@
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/ConvertPrefixIndexToList.js]] ([[File:User:Helder.wiki/Tools/ConvertPrefixIndexToList.js]])
  */
 if (mw.config.get('wgCanonicalSpecialPageName') === 'Prefixindex' ) {
-    $(function(){
-		var $table = $('#mw-prefixindex-list-table'),
+	$(function(){
+		var	$table = $('#mw-prefixindex-list-table'),
 			$list = $('<ol id="mw-prefixindex-list" style="-moz-column-count:3; -webkit-column-count:3; column-count:3;"></ol>');
 		$table.find('td').each(function() {
 			$list.append('<li>' + $(this).html() + '</li>');
 		});
 		$table.replaceWith($list);
-		if (typeof filterLists !== 'undefined' ) {
+		if ( typeof filterLists !== 'undefined' && $.isFunction(filterLists.load) && $.isFunction(filterLists.buildForm) ) {
 			filterLists.load();
 			filterLists.buildForm();
 		}

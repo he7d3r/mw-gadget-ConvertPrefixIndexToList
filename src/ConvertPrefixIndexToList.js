e7c807a6732a6c13a9d1b843ae28fad08c1503c6
Helder <he7d3r@gmail.com>
2014-08-09 19:19:16 +0000

    Fix the selector and enable on [[Special:AllPages]]
    The id "mw-prefixindex-list-table" is now a class. See:
    https://gerrit.wikimedia.org/r/#/c/100114/
    
    The same code also works on
    https://en.wikipedia.org/wiki/Special:AllPages

--- src/ConvertPrefixIndexToList.js
+++ src/ConvertPrefixIndexToList.js
@@ -8,19 +8,19 @@
 ( function ( mw, $ ) {
 'use strict';
 
-if (mw.config.get('wgCanonicalSpecialPageName') === 'Prefixindex' ) {
-	$(function(){
-		var	$table = $('#mw-prefixindex-list-table'),
-			$list = $('<ol id="mw-prefixindex-list" style="-moz-column-count:3; -webkit-column-count:3; column-count:3;"></ol>');
-		$table.find('td').each(function() {
-			$list.append('<li>' + $(this).html() + '</li>');
-		});
-		$table.replaceWith($list);
+if ( $.inArray( mw.config.get( 'wgCanonicalSpecialPageName' ), [ 'Prefixindex', 'Allpages' ] ) !== -1 ) {
+	$( function(){
+		var $table = $( '.mw-prefixindex-list-table, .mw-allpages-table-chunk' ),
+			$list = $( '<ol id="mw-prefixindex-list" style="-moz-column-count:3; -webkit-column-count:3; column-count:3;"></ol>' );
+		$table.find( 'td' ).each( function() {
+			$list.append( '<li>' + $( this ).html() + '</li>' );
+		} );
+		$table.replaceWith( $list );
 		if ( $.isFunction( window.filterLists && filterLists.load ) && $.isFunction( filterLists.buildForm ) ) {
 			filterLists.load();
 			filterLists.buildForm();
 		}
-	});
+	} );
 }
 
 }( mediaWiki, jQuery ) );

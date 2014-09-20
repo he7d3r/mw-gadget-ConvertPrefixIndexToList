0f7ecd1e11e5e6a161b951d7fac763a7b0741f2e
Helder <he7d3r@gmail.com>
2014-09-20 11:22:27 -0300

    Remove tracking link (only useful on wiki)

--- src/ConvertPrefixIndexToList.js
+++ src/ConvertPrefixIndexToList.js
@@ -2,7 +2,6 @@
  * Convert [[Special:PrefixIndex]] table to list and add the "lifilter", if available
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/ConvertPrefixIndexToList.js]] ([[File:User:Helder.wiki/Tools/ConvertPrefixIndexToList.js]])
  */
 /*jslint browser: true */
 /*global jQuery, mediaWiki, filterLists */

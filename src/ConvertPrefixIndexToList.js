a31e6c3dc63f0bbf752af0927827e07130e565a7
Helder <he7d3r@gmail.com>
2014-09-27 20:48:57 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/ConvertPrefixIndexToList.js
+++ src/ConvertPrefixIndexToList.js
@@ -4,21 +4,21 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-if ( $.inArray( mw.config.get( 'wgCanonicalSpecialPageName' ), [ 'Prefixindex', 'Allpages' ] ) !== -1 ) {
-	$( function(){
-		var $table = $( '.mw-prefixindex-list-table, .mw-allpages-table-chunk' ),
-			$list = $( '<ol id="mw-prefixindex-list" style="-moz-column-count:3; -webkit-column-count:3; column-count:3;"></ol>' );
-		$table.find( 'td' ).each( function() {
-			$list.append( '<li>' + $( this ).html() + '</li>' );
+	if ( $.inArray( mw.config.get( 'wgCanonicalSpecialPageName' ), [ 'Prefixindex', 'Allpages' ] ) !== -1 ) {
+		$( function () {
+			var $table = $( '.mw-prefixindex-list-table, .mw-allpages-table-chunk' ),
+				$list = $( '<ol id="mw-prefixindex-list" style="-moz-column-count:3; -webkit-column-count:3; column-count:3;"></ol>' );
+			$table.find( 'td' ).each( function () {
+				$list.append( '<li>' + $( this ).html() + '</li>' );
+			} );
+			$table.replaceWith( $list );
+			if ( $.isFunction( window.filterLists && filterLists.load ) && $.isFunction( filterLists.buildForm ) ) {
+				filterLists.load();
+				filterLists.buildForm();
+			}
 		} );
-		$table.replaceWith( $list );
-		if ( $.isFunction( window.filterLists && filterLists.load ) && $.isFunction( filterLists.buildForm ) ) {
-			filterLists.load();
-			filterLists.buildForm();
-		}
-	} );
-}
+	}
 
 }( mediaWiki, jQuery ) );
